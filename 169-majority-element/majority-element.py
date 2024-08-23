b2c4d95081a7329a8036ed8fdaf1b10012bf49fc
Santosh Kanumuri <73770283+santoshkanumuri@users.noreply.github.com>
2024-08-23 09:20:19 -0500

    Time: 174 ms (39.16%) | Memory: 18 MB (75.43%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,5 +1,20 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        ''' This program uses moore's algorithm to find the optimal solution for the problems which checks if the given number's
+        frequency in the array is greater than n/2 and returns that element.
+
+        intution:
+            we initially take first element as target and increase count to check how many time that number appeared. if the number is appeared then we increase 
+            count else if any other number appeared then we decrease the count. if we touch count to zero then we take the next element
+            as target and continue our iteration until iterating the complete array.
+
+            finally that algorithm states if a majority element exists in the array then this algorithm catches it, if not it will return a wrong element
+            so make sure to check the count of output is greater than n/2 before returning it.
+
+
+
+            in this given question they stated that element alays exist so i havent checked count for it.
+        '''
         count=0
         for num in nums:
             if(count==0):
