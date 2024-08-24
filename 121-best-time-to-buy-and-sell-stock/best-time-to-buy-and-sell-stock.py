393ef7c97411c681c41de16098a59c4155bdf249
Santosh Kanumuri <73770283+santoshkanumuri@users.noreply.github.com>
2024-08-24 11:28:20 -0500

    Time: 677 ms (93.14%) | Memory: 27.4 MB (34.77%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,8 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         max_diff=0
-        min_num=prices[0]
-        for num in prices:
-            min_num=min(num,min_num)
-            max_diff=max(max_diff,num-min_num)
+        min_price=prices[0]
+        for price in prices:
+            if(price<min_price):
+                min_price=price
+
+            if max_diff < price-min_price:
+                max_diff=price-min_price
+            
+
         return max_diff
