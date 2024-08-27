f1c81f0a85a54174bed8e4ea4db86cd028ccf178
Santosh Kanumuri <73770283+santoshkanumuri@users.noreply.github.com>
2024-08-27 09:33:19 -0500

    Time: 104 ms (47.79%) | Memory: 17.4 MB (85.22%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -0,0 +1,36 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        rows=len(matrix)
+        columns=len(matrix[0])
+        col0=1
+        '''use first row as the column marker and first column as row marker'''
+
+        for i in range(rows):
+            for j in range(columns):
+                if(matrix[i][j]==0):
+                    matrix[i][0]=0
+                    if(j!=0):
+                        matrix[0][j]=0
+                    else:
+                        col0=0
+        '''start modifying matrix from second row and second column '''
+        for i in range(1,rows):
+            for j in range(1,columns):
+                if(matrix[i][0]==0 or matrix [0][j]==0):
+                    matrix[i][j]=0
+
+        ''' mosify first row from last since it is dependent on first column'''
+        if(matrix[0][0]==0):
+            for j in range(1,columns):
+                matrix[0][j]=0
+
+        '''finally modify first column'''
+        if(col0==0):
+            for i in range(rows):
+                matrix[i][0]=0
+        return matrix
+
+
+
+                    
+        
