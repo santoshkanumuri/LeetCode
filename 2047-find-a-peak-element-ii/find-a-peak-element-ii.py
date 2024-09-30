bb778b28589df52091ea1b7dfb31eab696a5b8fb
Santosh Kanumuri <73770283+santoshkanumuri@users.noreply.github.com>
2024-09-30 09:48:07 -0500

    Time: 904 ms (46.04%) | Memory: 48 MB (15.62%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.py
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.py
@@ -1,40 +1,30 @@
 class Solution:
     def findPeakGrid(self, mat: List[List[int]]) -> List[int]:
-        def find(mat,i,j):
+        m:int=len(mat)
+        n:int=len(mat[0])
+        low:int=0
+        high:int=n-1
+        while(low<=high):
             l:int=-1
             r:int=-1
-            u:int=-1
-            d:int=-1
-            c:int=0
-            m:int=len(mat)
-            n:int=len(mat[0])
-            print(i,j)
+            mid=(low+high)//2
+            mx,mx_ind=mat[0][mid],0
+            for i in range(m):
+                if(mx<mat[i][mid]):
+                    mx_ind=i
+                    mx=mat[i][mid]
+            print(mx_ind,mid)
+            if(mid!=0):
+                l=mat[mx_ind][mid-1]
+            if(mid<n-1):
+                r=mat[mx_ind][mid+1]
+            if(mat[mx_ind][mid]>l and mat[mx_ind][mid]>r):
+                print(mat[mx_ind][mid],l,r)
+                return [mx_ind,mid]
+            elif(mat[mx_ind][mid]<=l):
+                high=mid-1
+            else:
+                low=mid+1
             
 
-            if(not i==0):
-                u=mat[i-1][j]
-            if(not j==0):
-                l=mat[i][j-1]
-            if(i!=m-1):
-                d=mat[i+1][j]
-            if(j!=n-1):
-                r=mat[i][j+1]
-            print(l,r,u,d)
-            for ele in [l,r,u,d]:
-                if(mat[i][j]>ele):
-                    c+=1
-
-            return c==4
-
-        
-
-        m=len(mat)
-        n=len(mat[0])
-        for i in range(m):
-            for j in range(n):
-                if(find(mat,i,j)):
-                    return [i,j]
-
-
-
-        
+            
