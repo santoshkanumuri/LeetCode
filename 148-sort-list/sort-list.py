4ab885ac631b025de51f2ab4a011b3517747e8c4
Santosh Kanumuri <73770283+santoshkanumuri@users.noreply.github.com>
2024-10-20 11:46:24 -0500

    Time: 185 ms (74.63%) | Memory: 31.8 MB (99.17%) - LeetSync

--- 148-sort-list/sort-list.py
+++ 148-sort-list/sort-list.py
@@ -5,16 +5,39 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        li=[]
-        cur=head
-        while cur:
-            li.append(cur.val)
-            cur=cur.next
-        li.sort()
-        cur=head
-        i=0
-        while cur:
-            cur.val=li[i]
-            cur=cur.next
-            i+=1
-        return head
+        if not head or not head.next:
+            return head
+        
+        left=head
+        right=self.getmid(left)
+        tmp=right.next
+        right.next=None
+        right=tmp
+
+        left=self.sortList(left)
+        right=self.sortList(right)
+        return self.merge(left,right)
+
+    def getmid(self,head):
+        s,f=head,head.next
+        while f and f.next:
+            f=f.next.next
+            s=s.next
+        return s
+
+    def merge(self,left,right):
+        tail=dummy=ListNode(0)
+        while left and right:
+            if left.val<right.val:
+                tail.next=left
+                left=left.next
+            else:
+                tail.next=right
+                right=right.next
+            tail=tail.next
+        if left:
+            tail.next=left
+        if right:
+            tail.next=right
+        return dummy.next
+    
