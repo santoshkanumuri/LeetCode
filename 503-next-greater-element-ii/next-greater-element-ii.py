7746b70b7261487b5510473f766711ae1ec5c7d3
Santosh Kanumuri <73770283+santoshkanumuri@users.noreply.github.com>
2024-11-02 10:37:53 -0500

    Time: 31 ms (31.92%) | Memory: 19.1 MB (5.31%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.py
+++ 503-next-greater-element-ii/next-greater-element-ii.py
@@ -3,7 +3,6 @@ class Solution:
         n=len(nums)
         res=[-1]*n
         nge=nums+nums
-        print(nge)
         stack=[]
         for i in range(len(nge)):
             while stack and stack[-1][0]<nge[i]:
