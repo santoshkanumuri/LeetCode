589dc94196e46418b3ff02c2633b5e9409574119
Santosh Kanumuri <73770283+santoshkanumuri@users.noreply.github.com>
2024-11-02 10:16:38 -0500

    Time: 3 ms (12.78%) | Memory: 16.5 MB (80.88%) - LeetSync

--- 2580-circular-sentence/circular-sentence.py
+++ 2580-circular-sentence/circular-sentence.py
@@ -1,11 +1,11 @@
 class Solution:
     def isCircularSentence(self, sentence: str) -> bool:
-        li=list(sentence.strip().split())
         if not sentence[-1]==sentence[0]:
             return False
-        for i in range(len(li)-1):
-            print(li[i])
-            if li[i][-1]!=li[i+1][0]:
-                return False
+        for i in range(len(sentence)):
+            char=sentence[i]
+            if char==" ":
+                if sentence[i-1]!=sentence[i+1]:
+                    return False
         return True
         
