636816ecbcd567fa874e644c062520baf6e4e8cf
Santosh Kanumuri <73770283+santoshkanumuri@users.noreply.github.com>
2024-11-10 11:53:09 -0600

    Time: 13 ms (10.06%) | Memory: 16.7 MB (10.60%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -4,14 +4,10 @@ class Solution:
         mp={"I":1,"IV":4,"V":5,"IX":9,"X":10,"XL":40,"L":50,"XC":90,"C":100,"CD":400,"D":500,"CM":900,"M":1000}
         chars=list(mp.keys())[::-1]
         nums=list(mp.values())[::-1]
-        while num>=1:
-            print(num,out)
-            for i in range(13):
-                n=nums[i]
-                if num>=n:
-                    num=num-n
-                    out=out+chars[i]
-                    break
+        for i in range(13):
+            res=num//nums[i]
+            out=out+res*chars[i]
+            num=num%nums[i]
         return out
        
 
