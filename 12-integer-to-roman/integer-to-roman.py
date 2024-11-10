ae2b02ca6b50c35ef5162cb96a356f1df7eb212b
Santosh Kanumuri <73770283+santoshkanumuri@users.noreply.github.com>
2024-11-10 11:57:33 -0600

    Time: 9 ms (24.09%) | Memory: 16.9 MB (10.60%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -1,9 +1,8 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
         out=""
-        mp={"I":1,"IV":4,"V":5,"IX":9,"X":10,"XL":40,"L":50,"XC":90,"C":100,"CD":400,"D":500,"CM":900,"M":1000}
-        chars=list(mp.keys())[::-1]
-        nums=list(mp.values())[::-1]
+        nums=[1000,900,500,400,100,90,50,40,10,9,5,4,1]
+        chars=["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
         for i in range(13):
             res=num//nums[i]
             out=out+res*chars[i]
