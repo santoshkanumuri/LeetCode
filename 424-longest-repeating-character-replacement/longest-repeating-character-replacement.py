089f73fe2c4f9c00008871f161da66afc544c492
Santosh Kanumuri <73770283+santoshkanumuri@users.noreply.github.com>
2024-11-14 11:09:12 -0600

    Time: 83 ms (33.09%) | Memory: 16.8 MB (23.37%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -0,0 +1,23 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        maxlen=0
+        maxf=0
+        l=0
+        hm=[0]*26
+        n=len(s)
+        for r in range(n):
+            hm[ord(s[r])-65]+=1
+            maxf=max(maxf,hm[ord(s[r])-65])
+            change=(r-l+1)-maxf
+            if change<=k:
+                maxlen=max(maxlen,r-l+1)
+            else:
+                while (r-l+1-maxf>k):
+                    hm[ord(s[l])-65]-=1
+                    l+=1
+                    maxf=max(hm)
+            maxlen=max(maxlen,r-l+1)
+        return maxlen
+            
+
+
