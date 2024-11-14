74a0194a71fe1dbaa92fa796773e54f6b2dc5f06
Santosh Kanumuri <73770283+santoshkanumuri@users.noreply.github.com>
2024-11-14 11:46:12 -0600

    Time: 51 ms (61.38%) | Memory: 16.7 MB (63.00%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -3,18 +3,14 @@ class Solution:
         maxlen=0
         maxf=0
         l=0
-        hm=[0]*26
-        n=len(s)
-        for r in range(n):
-            hm[ord(s[r])-65]+=1
-            maxf=max(maxf,hm[ord(s[r])-65])
+        hm={}
+        for r in range(len(s)):
+            hm[s[r]]=1+hm.get(s[r],0)
+            maxf=max(maxf,hm[s[r]])
             change=(r-l+1)-maxf
-            if change<=k:
-                maxlen=max(maxlen,r-l+1)
-            else:
-                while (r-l+1-maxf>k):
-                    hm[ord(s[l])-65]-=1
-                    l+=1
+            while (r-l+1-maxf>k):
+                hm[s[l]]-=1
+                l+=1
             maxlen=max(maxlen,r-l+1)
         return maxlen
             
