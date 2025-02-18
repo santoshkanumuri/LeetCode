84f270f1ac6b31bc8fe4eb89cf0602d204052caf
Santosh Kanumuri <73770283+santoshkanumuri@users.noreply.github.com>
2025-02-18 11:16:00 -0600

    Time: 0 ms (100.00%) | Memory: 18.1 MB (24.35%) - LeetSync

---
+++ 257-binary-tree-paths/binary-tree-paths.py
@@ -0,0 +1,42 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        if not root:
+            return []
+        
+        paths=[]
+
+        def is_leaf(root):
+            if not (root.left or root.right):
+                return True
+        
+        def traverse(root,path):
+            if not root:
+                return False
+            
+            path.append(str(root.val))
+            
+            if is_leaf(root):
+                print(path)
+                k=path.copy()
+                paths.append(k)
+                
+            if traverse(root.left, path):
+                return True
+            if traverse(root.right, path):
+                return True
+            path.pop()
+            return False
+        
+        traverse(root,[])
+        res=[]
+        paths=["->".join(path) for path in paths]
+        return paths
+
+
+        
